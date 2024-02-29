f84721703ee86c74416c3f516ed0468864213370
Fevralev Dmitriy <dnfevralev@sber.ru>
2024-02-29 16:42:30 +0300

    hw

---
+++ src/main/java/ru/edu/App.java
@@ -0,0 +1,12 @@
+package ru.edu;
+
+import org.apache.commons.lang.StringUtils;
+
+public class App {
+
+    static {
+        System.out.println(StringUtils.center("not main", 50, '*'));
+    }
+
+    public static void main(String[] args) {}
+}
